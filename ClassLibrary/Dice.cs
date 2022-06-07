b9775b68fd39577f912cff9fe8744432365935e5
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-06-08 00:45:34 +0200

    added dice class k6

---
+++ ClassLibrary/Dice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Dice
+    {
+        private static Dice dice;
+        public static Dice GetInstance()
+        {
+            if (dice == null)
+            {
+                dice = new Dice();
+            }
+            return dice;
+        }
+        Random random;
+        private Dice()
+        {
+            random = new Random();
+        }
+        public int RollK6()
+        {
+            return random.Next(1, 7);
+        }
+    }
+}
