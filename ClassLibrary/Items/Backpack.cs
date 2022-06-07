6e2048c058c33b8d10f19694094412d8d437a537
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-06-07 23:31:45 +0200

    added backpack class

---
+++ ClassLibrary/Items/Backpack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary.Items
+{
+    public class Backpack : Item, ILiftable, IStorage
+    {
+        List<ILiftable> items = new List<ILiftable>();
+        public float Weight { get; }
+        public float Capacity { get { return items.Sum(x => x.Weight); } }
+        public float MaxCapacity { get; }
+        public Backpack(float weight, float maxCapacity, string name, string description) : base(name, description)
+        {
+            Weight = weight;
+            MaxCapacity = maxCapacity;
+        } 
+        public bool PutInside(ILiftable item)
+        {
+            if (item.Weight + Capacity <= MaxCapacity)
+            {
+                items.Add(item);
+                return true;
+            }
+            else return false;
+        }
+    }
+}
