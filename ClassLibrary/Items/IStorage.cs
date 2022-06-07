81e8da3089227e2c0783794bf28bd1d4e541b122
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-06-07 23:32:14 +0200

    added istorage interface

---
+++ ClassLibrary/Items/IStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Items
+{
+    public interface IStorage
+    {
+        float Capacity { get; }
+        float MaxCapacity { get; }
+    }
+}
