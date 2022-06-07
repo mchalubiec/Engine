aba86fc21b53e05fd64ed6b147e7618d38939260
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-06-08 00:45:52 +0200

    added weapon one handed class and interface

---
+++ ClassLibrary/Items/Weapons/Interfaces/IWeaponOneHanded.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Items.Weapons.Interfaces
+{
+    public interface IWeaponOneHanded
+    {
+        int Damage { get; }
+    }
+}
---
+++ ClassLibrary/Items/Weapons/Models/WeaponOneHanded.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Items.Weapons.Models
+{
+    public class WeaponOneHanded : Item, ILiftable
+    {
+        public float Weight { get;  }
+        public WeaponOneHanded(string name, string description, float weight) : base(name, description)
+        {
+            Weight = weight;
+        }
+    }
+}
