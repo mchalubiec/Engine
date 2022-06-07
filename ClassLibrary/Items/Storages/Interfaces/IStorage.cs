8aca055fc29676ab19846682fcb5c0fbf045da1d
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-06-08 00:46:13 +0200

    changed name of class

---
+++ ClassLibrary/Items/Storages/Interfaces/IStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Items.Storages.Interfaces
+{
+    public interface IStorage
+    {
+        float Capacity { get; }
+        float MaxCapacity { get; }
+    }
+}
---
+++ ClassLibrary/Items/Storages/Models/Storage.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary.Items.Storages.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibrary.Items.Storages.Models
+{
+    public class Storage : Item, ILiftable, IStorage
+    {
+        List<ILiftable> items = new List<ILiftable>();
+        public float Weight { get; }
+        public float Capacity { get { return items.Sum(x => x.Weight); } }
+        public float MaxCapacity { get; }
+        public Storage(float weight, float maxCapacity, string name, string description) : base(name, description)
+        {
+            Weight = weight;
+            MaxCapacity = maxCapacity;
+        } 
+        public bool PutInside(ILiftable item)
+        {
+            if (item.Weight + Capacity <= MaxCapacity)
+            {
+                items.Add(item);
+                return true;
+            }
+            else return false;
+        }
+        public void BrowseInside()
+        {
+
+        }
+    }
+}
