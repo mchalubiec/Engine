37bba1e43290e3268b5a97ab6d6cf9e0b4147408
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-06-07 23:00:41 +0200

    added item class

---
+++ ClassLibrary/Items/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Items
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Item(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
