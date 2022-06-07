366b8182a782900b3ad680f734d8672268335b7b
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-06-07 23:32:02 +0200

    added iliftable interface

---
+++ ClassLibrary/Items/ILiftable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Items
+{
+    public interface ILiftable
+    {
+        float Weight { get; }
+    }
+}
